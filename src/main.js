1fe4e06918b85048d95423c4af6e5ec64889f518
Charles Julian Knight <charles@rabidaudio.com>
2013-12-15 16:36:01 -0500

    patched a few bugs, refactored

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@
 
 var running = false;
 var filepath = null;
+var tab_num = {};
+var danger_detected = false;
 
 function getTimeStamp() {
     var now = new Date();
@@ -22,33 +24,68 @@ function getTimeStamp() {
                  : (now.getSeconds())));
 }
 
+function pad(filename){
+    var str = '' + (tab_num[filename] || 0);
+    while (str.length < 3) {
+        str = '0' + str;
+    }
+    return str;
+}
+
 function listenAndOpenWhenDone(id){
     chrome.downloads.onChanged.addListener(function(delta) {
         if (!delta.state ||  (delta.state.current != 'complete'))
             return;
         if(running){
             running=false;
+            danger_detected=false;
+            tab_num = {};
             chrome.downloads.show(id);
             console.log("DownloadTabs - done.");
             filepath = null;
         }
     });
 }
-      
 
-function main(){
+function accept_if_dangerous(downid){
+  //TODO this isn't perfect. It actually takes longer than clicking keep in the bottom bar. Either fix acceptDanger
+  //    to show popups, or maybe if it is detected as dangerous, cancel download and restart as SaveAs... so dialog comes up
+  chrome.downloads.search({id: downid}, function(results){
+  	var r = results[0];
+    //console.log("accept if dangerous called: " + r.url+ " =>" +r.danger + " (danger detected:"+danger_detected+")");
+    if (danger_detected ||
+        (r.state != 'in_progress') ||
+        (r.danger == 'safe') ||
+        (r.danger == 'accepted')
+       ){
+      return;
+    }
+    danger_detected = true;
+    console.log("DownloadTabs - Dangerous file detected. Opening Chrome downloads page for approval");
+    chrome.tabs.create({url: "chrome://downloads"} );//, function(tab){console.log("Downloads tab opened for acceptance");});
+    /*chrome.downloads.acceptDanger(r.id, function(){
+      console.log("DownloadTabs - Note: accepted warning for file " + r.url + " (" + r.danger+")");
+      accept_if_dangerous(id); //call again to be sure
+    });*/
+  });
+}
+
+function main(wid){
     running = true;
-    var q = chrome.tabs.query({ currentWindow: true }, function(result){
+    var q = chrome.tabs.query({ windowId: wid }, function(result){
         var count = 0;
+        tab_num = {};
         for (var i=0; i<result.length; i++){
             console.log("DownloadTabs - Downloading "+result[i].url);
+            var filename = result[i].url.replace(/.*\//, "");
+            tab_num[filename] = i;
             chrome.downloads.download({url: result[i].url}, function(id){
                 count++;
+                //accept_if_dangerous(id); 
+                window.setTimeout(function(){ accept_if_dangerous(id); }, 500);//the delay allows it to check the file first. TODO add listener instead
                 if(count==result.length){
                     listenAndOpenWhenDone(id);
                     if( confirm('All done. Close the window?') ){
-                            //window.close();
-                            //chrome.windows.remove(chrome.windows.WINDOW_ID_CURRENT);
                             for(var j=0;j<result.length;j++) chrome.tabs.remove(result[j].id);
                     }
                 }
@@ -59,13 +96,11 @@ function main(){
 
 
 chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
-  console.log("download listener cought this - "+item.filename);
-  if(!filepath)
-    filepath = "TabDownload-"+getTimeStamp()+"/";
-  //console.log("suggesting:"+filepath+item.filename);
+    if(!filepath)
+        filepath = "TabDownload-"+getTimeStamp()+"/";
   if(running){
     console.log('DownloadTabs - Saving to '+filepath+item.filename);
-      suggest({filename: filepath+item.filename,
+      suggest({filename: filepath+pad(item.filename)+"-"+item.filename,
                conflict_action: 'overwrite',
                conflictAction: 'overwrite'});
    }
@@ -75,7 +110,10 @@ chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
 
 chrome.browserAction.onClicked.addListener(function(tab) {
     console.log('DownloadTabs - Beginning process...');
-    main();
+    //chrome.tabs.getCurrent(function callback(tab)) //tab.windowId || windows.WINDOW_ID_CURRENT //query {windowId:}
+    //main();
+    var wid = tab.windowId || windows.WINDOW_ID_CURRENT;
+    main(wid);
 });
 
 // TODO if filepath is null, set saveas in download command to true. then store
